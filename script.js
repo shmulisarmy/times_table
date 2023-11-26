093d41e4d41dd3d81e57fb135b64ba01e03d6615
shmulisarmy <shmulikeller@gmail.com>
2023-11-25 23:48:31 -0500

    ..

---
+++ script.js
@@ -0,0 +1,28 @@
+function submit(){
+    console.log('clicked');
+    let table = document.getElementById('myTable');
+    var input = document.getElementById('input');
+    var table_size = parseInt(input.value);
+    if (table_size > 40){
+        return;
+    }
+    input.value = '';
+    table.textContent = '';
+
+    for (var row = 1; row <= table_size; row++){
+        var html_row = document.createElement('tr');
+    for (var col = 1; col <= table_size; col++){
+        var html_col = document.createElement('th');
+        // html_col.setAttribute('width', '30px');
+        html_col.textContent = row*col;
+        html_row.appendChild(html_col);
+        
+        console.log(row*col);
+} 
+table.appendChild(html_row);
+    
+}};
+window.addEventListener('keydown', key => {
+    if (key.code === "Enter"){
+        submit();
+}})
